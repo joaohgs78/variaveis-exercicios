ab676131358851e56a7488c5d86f6bb55dc58949
joaohgs78 <102187505+joaohgs78@users.noreply.github.com>
2023-01-31 19:30:55 -0300

    resolução dos exercicios

---
+++ exercicio1/script.js
@@ -0,0 +1,56 @@
+// const nome
+// let idade
+// console.log(typeof nome)
+// console.log(typeof idade)
+// o tipo undefield aparece pos eu nao define valores para as variaveis 
+
+
+// const nome = 'Joao Lucas'
+// let idade = 26
+// console.log('Qual o seu nome ?', nome)
+// console.log('Qual sua idade?', idade)
+
+
+//  const nome = 'Joao Lucas'
+//  let idade = 26
+//  console.log(typeof nome)
+//  console.log(typeof idade)
+
+//   //a variavel nome é uma string
+//   //a variavel idade é um number
+
+//   console.log('Olá', nome, 'voce tem', idade, 'anos')
+
+
+// const nome = prompt('Qual o seu nome?')
+// let idade = prompt('Qual sua idade?')
+// let cidade = prompt('Qual cidade voce mora ?')
+
+// console.log('Resposta ' + nome)
+// console.log('Resposta ' + idade)
+// console.log('Resposta ' + cidade)
+
+
+// const nomeA = 'Qual o seu nome?'
+// let idadeA= 'Qual sua idade?'
+// let cidadeA = 'Qual sua cidade?'
+
+
+// const nome = prompt('Qual o seu nome?')
+// let idade = prompt('Qual sua idade?')
+// let cidade = prompt('Qual sua cidade?')
+
+// console.log('Resposta ' + nome)
+// console.log('Resposta ' + idade)
+// console.log('Resposta ' + cidade)
+
+
+
+
+
+
+
+
+
+
+
