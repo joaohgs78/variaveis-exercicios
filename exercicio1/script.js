6fd36df9fa23c381d6018cbc9b3370cbf95413fb
joaohgs78 <102187505+joaohgs78@users.noreply.github.com>
2023-01-31 21:40:08 -0300

    resoluçao dos exercicios

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -1,42 +1,57 @@
-// const nome
-// let idade
-// console.log(typeof nome)
-// console.log(typeof idade)
-// o tipo undefield aparece pos eu nao define valores para as variaveis 
+ /* let nome
+ let idade
+ console.log(typeof nome)
+ console.log(typeof idade) */
+ /* o tipo undefield aparece pos eu nao define valores para as variaveis 
+ */
+
+ /*
+ nome = prompt('Qual o seu nome?')
+ idade = Number(prompt('Qual o seu idade?'))
+ console.log(typeof (nome), typeof idade )
+
+ */
+
+/*
+console.log('Olá', nome, 'voce tem', idade, 'anos')
+
+console.log(` Olá, ${nome}, voce tem ${idade}. anos`)
 
 
-// const nome = 'Joao Lucas'
-// let idade = 26
-// console.log('Qual o seu nome ?', nome)
-// console.log('Qual sua idade?', idade)
+console.log('Olá' + nome + 'voce tem' + idade + 'anos')
 
 
-//  const nome = 'Joao Lucas'
-//  let idade = 26
-//  console.log(typeof nome)
-//  console.log(typeof idade)
-
-//   //a variavel nome é uma string
-//   //a variavel idade é um number
-
-//   console.log('Olá', nome, 'voce tem', idade, 'anos')
 
 
-// const nome = prompt('Qual o seu nome?')
-// let idade = prompt('Qual sua idade?')
-// let cidade = prompt('Qual cidade voce mora ?')
-
-// console.log('Resposta ' + nome)
-// console.log('Resposta ' + idade)
-// console.log('Resposta ' + cidade)
+*/
+// pula linha no console \n
+//console.log("Minha idade é \n idade:", idade)
 
 
-// const nomeA = 'Qual o seu nome?'
+
+
+
+
+
+
+
+let nome = prompt('Qual o seu nome?')
+let idade = prompt('Qual sua idade?')
+let cidade = prompt('Qual cidade voce mora ?')
+
+console.log('Resposta ' + nome)
+console.log('Resposta ' + idade)
+console.log('Resposta ' + cidade)
+
+
+
+
+// let nomeA = 'Qual o seu nome?'
 // let idadeA= 'Qual sua idade?'
 // let cidadeA = 'Qual sua cidade?'
 
 
-// const nome = prompt('Qual o seu nome?')
+// let nome = prompt('Qual o seu nome?')
 // let idade = prompt('Qual sua idade?')
 // let cidade = prompt('Qual sua cidade?')
 
